99563f4e93a0aff90522fa7bdf80e328526f2cc7
Daniel <danieel47896321@gmail.com>
2022-12-02 11:14:47 +0200

    Create CreateAccount,SignIn and ResetPassword Pages. Finish MainActivity Page.

---
+++ app/src/main/java/com/example/worktracking/Guest/ResetPassword.java
@@ -0,0 +1,16 @@
+package com.example.worktracking.Guest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.worktracking.R;
+
+public class ResetPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
--- app/src/main/java/com/example/worktracking/MainActivity.java
+++ app/src/main/java/com/example/worktracking/MainActivity.java
@@ -1,14 +1,77 @@
 package com.example.worktracking;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.worktracking.Guest.CreateAccount;
+import com.example.worktracking.Guest.SignIn;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView Title;
+    private ImageView BackIcon;
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private String[] titles;
+    private final int SIZE = 2;
+    private PagerAdapter pagerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Init();
+    }
+    private void Init(){
+        setID();
+        setPager();
+    }
+    private void setID(){
+        Title = findViewById(R.id.Title);
+        Title.setText("");
+        BackIcon = findViewById(R.id.BackIcon);
+        BackIcon.setVisibility(View.GONE);
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager2 = findViewById(R.id.viewPager2);
+        titles = new String[SIZE];
+        titles[0] = getResources().getString(R.string.SignIn);
+        titles[1] = getResources().getString(R.string.CreateAccount);
+    }
+    private void setPager(){
+        pagerAdapter = new PagerAdapter(MainActivity.this);
+        viewPager2.setAdapter(pagerAdapter);
+        new TabLayoutMediator(tabLayout,viewPager2, ((tab, position) -> tab.setText(titles[position]))).attach();
+    }
+    public static class PagerAdapter extends FragmentStateAdapter{
+        private final int SIZE = 2;
+        public PagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
+        }
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch (position){
+                case 0:
+                    return new SignIn();
+                case 1:
+                    return new CreateAccount();
+            }
+            return new SignIn();
+        }
+        @Override
+        public int getItemCount() { return SIZE; }
     }
 }
