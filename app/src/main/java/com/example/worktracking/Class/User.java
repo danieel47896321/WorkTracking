d5889b2b8cdb71a471d39bb44844832e12e57382
Daniel <danieel47896321@gmail.com>
2022-12-02 11:42:12 +0200

    Finish SignIn xml.

---
+++ app/src/main/java/com/example/worktracking/Class/User.java
@@ -0,0 +1,39 @@
+package com.example.worktracking.Class;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String Uid = "Uid";
+    private String Email = "test@email.com";
+    private String FirstName = "FirstName";
+    private String LastName = "LastName";
+    public User(String email, String firstName, String lastName) {
+        Email = email;
+        FirstName = firstName;
+        LastName = lastName;
+    }
+    public String getUid() {
+        return Uid;
+    }
+    public void setUid(String uid) {
+        Uid = uid;
+    }
+    public String getEmail() {
+        return Email;
+    }
+    public void setEmail(String email) {
+        Email = email;
+    }
+    public String getFirstName() {
+        return FirstName;
+    }
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+    public String getLastName() {
+        return LastName;
+    }
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+}
